6d66b40636c7a465f8c871358063bd5dc9068d44
Taylor Phelps <tphelps1993@gmail.com>
2021-02-22 19:14:38 -0500

    Updates

---
+++ precache-manifest.230993f59f2b180756faff4a523809bc.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b47539926d154f39b6633b0687547d8",
+    "url": "/tphelps93/fed-challenge-starter/index.html"
+  },
+  {
+    "revision": "7d47752ad435f1838f4f",
+    "url": "/tphelps93/fed-challenge-starter/static/css/main.520a7b8a.chunk.css"
+  },
+  {
+    "revision": "a9c1dbd6c9d2ed10ae5e",
+    "url": "/tphelps93/fed-challenge-starter/static/js/2.ea600fdb.chunk.js"
+  },
+  {
+    "revision": "f75561adb7f4db3b610232e7231a7ed4",
+    "url": "/tphelps93/fed-challenge-starter/static/js/2.ea600fdb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d47752ad435f1838f4f",
+    "url": "/tphelps93/fed-challenge-starter/static/js/main.b931f8e2.chunk.js"
+  },
+  {
+    "revision": "8654f986d05f70a9700e",
+    "url": "/tphelps93/fed-challenge-starter/static/js/runtime-main.2f101b87.js"
+  },
+  {
+    "revision": "1542c1188204879edee18cf6e115d934",
+    "url": "/tphelps93/fed-challenge-starter/static/media/20-minutes-to-toned-thumb.1542c118.jpg"
+  },
+  {
+    "revision": "003901761c4b2f91b5c1b144747d4e83",
+    "url": "/tphelps93/fed-challenge-starter/static/media/charles-race-thumb.00390176.jpg"
+  },
+  {
+    "revision": "578dacbc0d22c3d6ff585347dadb0e73",
+    "url": "/tphelps93/fed-challenge-starter/static/media/full-body-hiit-thumb.578dacbc.jpg"
+  },
+  {
+    "revision": "a55f946a95a88b198f5fb3f1173a95f6",
+    "url": "/tphelps93/fed-challenge-starter/static/media/kafue-river-thumb.a55f946a.jpg"
+  },
+  {
+    "revision": "e0a7c9061a0e72bed419a8b9eaf2b286",
+    "url": "/tphelps93/fed-challenge-starter/static/media/lake-inniscarra-thumb.e0a7c906.jpg"
+  },
+  {
+    "revision": "3e82c42342a27af7cce1e89930288333",
+    "url": "/tphelps93/fed-challenge-starter/static/media/performance-series-thumb.3e82c423.jpg"
+  },
+  {
+    "revision": "04edd5427547b03287f6d75cfabea837",
+    "url": "/tphelps93/fed-challenge-starter/static/media/shred-and-burn-thumb.04edd542.jpg"
+  },
+  {
+    "revision": "7cae9ed133157efe82011594ed819bcc",
+    "url": "/tphelps93/fed-challenge-starter/static/media/slow-pulls-thumb.7cae9ed1.jpg"
+  }
+]);
